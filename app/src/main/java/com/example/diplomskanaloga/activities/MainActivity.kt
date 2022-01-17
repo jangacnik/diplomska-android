9e64cd1b3fa4aeb58a392b2f441e9f65fea6778e
Jan <gacnik.jan@gmail.com>
2022-01-17 22:33:07 +0100

    Updated yearly overview to linechart

--- app/src/main/java/com/example/diplomskanaloga/activities/MainActivity.kt
+++ app/src/main/java/com/example/diplomskanaloga/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diplomskanaloga.activities
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,10 +13,12 @@ import com.example.diplomskanaloga.interfaces.VolleyResponse
 import com.example.diplomskanaloga.models.Employee
 import com.example.diplomskanaloga.services.EmployeeRestService
 import com.example.diplomskanaloga.utils.ChartUtils
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.charts.HorizontalBarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.*
 import com.google.gson.Gson
 
 
@@ -33,7 +36,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         welcomeTextView = findViewById(R.id.welcome_text)
         employeeRestService = EmployeeRestService()
-        isUserLoggedIn()
+        barchartTest(R.id.weekly_bar_chart)
+        lineChartTest()
     }
 
     fun isUserLoggedIn() {
@@ -51,7 +55,6 @@ class MainActivity : AppCompatActivity() {
                     welcomeTextView.append(", "+userData.name)
                     Log.w("Response", userData.toString())
                 }
-
                 override fun onError(error: VolleyError?) {
                     if (error != null) {
                         Log.e("Response Error", error.networkResponse.toString())
@@ -61,23 +64,85 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun barTest() {
+    fun barchartTest(id: Int) {
         val entries:MutableList<BarEntry> = ArrayList()
         entries.add(BarEntry(0f, 30f))
-        entries.add(BarEntry(1f, 20f))
+        entries.add(BarEntry(1f, 120f))
         entries.add(BarEntry(2f, 50f))
         entries.add(BarEntry(3f, 60f))
         entries.add(BarEntry(4f, 10f))
         entries.add(BarEntry(5f, 70f))
-
         val barData = BarDataSet(entries, null)
         val data = BarData(barData)
         data.barWidth = 0.9f
-        val chart: BarChart = ChartUtils.setBarChartDefaults( findViewById<BarChart>(R.id.weekly_bar_chart2))
+        val chart: BarChart = ChartUtils.setBarChartDefaults( findViewById<BarChart>(id))
         chart.data = data
+        chart.animateXY(1000,1000)
         // set text size of bar values
         chart.data.setValueTextSize(14f)
+        chart.animate()
+    }
 
+    fun horizontalBarChartText(id: Int) {
+        val entries:MutableList<BarEntry> = ArrayList()
+        entries.add(BarEntry(0f, 12f))
+        entries.add(BarEntry(1f, 32f))
+        entries.add(BarEntry(2f, 4f))
+        entries.add(BarEntry(3f, 15f))
+        entries.add(BarEntry(4f, 14f))
+        entries.add(BarEntry(5f, 71f))
+        entries.add(BarEntry(6f, 71f))
+        entries.add(BarEntry(7f, 10f))
+        entries.add(BarEntry(8f, 174f))
+        entries.add(BarEntry(9f, 11f))
+        entries.add(BarEntry(10f, 11f))
+        entries.add(BarEntry(11f, 11f))
+        entries.reverse()
+        val barData = BarDataSet(entries, null)
+        barData.setDrawValues(true)
+        barData.valueTextSize = 14f
+        barData.isHighlightEnabled = true
+        val data = BarData(barData)
+        data.setDrawValues(true)
+        data.setValueTextColor(Color.BLACK)
+        data.barWidth = 0.9f
+        val chart: BarChart = ChartUtils.setHorizontalBarChartDefault( findViewById<HorizontalBarChart>(id))
+
+        chart.data = data
+        chart.setMaxVisibleValueCount(12)
+        chart.xAxis.setLabelCount(entries.size, false)
+
+        chart.setDrawValueAboveBar(false)
+        chart.data.setValueTextColor(Color.BLACK)
+        chart.xAxis.labelCount = entries.size
+        // set text size of bar values
+        chart.axisLeft.axisMaximum = 250f
+        chart.axisLeft.axisMinimum = 0f
+        chart.animateXY(1000,1000)
+        chart.setDrawValueAboveBar(true)
+        chart.invalidate()
+        chart.refreshDrawableState()
+    }
+
+    fun lineChartTest() {
+
+        val entries:MutableList<Entry> = ArrayList()
+        entries.add( Entry(0f, 44f))
+        entries.add( Entry(1f, 11f))
+        entries.add( Entry(2f, 22f))
+        entries.add( Entry(3f, 34f))
+        entries.add( Entry(4f, 47f))
+        val lineDataSet = LineDataSet(entries, "test")
+        lineDataSet.lineWidth = 4f
+        lineDataSet.color = Color.CYAN
+        lineDataSet.setDrawCircles(false)
+        lineDataSet.setDrawHighlightIndicators(true)
+        lineDataSet.isHighlightEnabled = true
+        lineDataSet.highLightColor = Color.CYAN
+        lineDataSet.valueTextColor = Color.DKGRAY
+        lineDataSet.mode = LineDataSet.Mode.STEPPED
+        val chart = findViewById<LineChart>(R.id.yearly_overview_chart)
+        chart.data = LineData(lineDataSet)
         chart.invalidate()
     }
 }
--- app/src/main/java/com/example/diplomskanaloga/utils/ChartUtils.kt
+++ app/src/main/java/com/example/diplomskanaloga/utils/ChartUtils.kt
@@ -1,7 +1,9 @@
 package com.example.diplomskanaloga.utils
 
+import android.graphics.Color
 import com.example.diplomskanaloga.Constants
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.HorizontalBarChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
@@ -29,6 +31,30 @@ class ChartUtils {
             // hide legend
             chart.legend.isEnabled = false
             chart.setFitBars(false)
+            return chart
+        }
+
+        fun setHorizontalBarChartDefault(chart: HorizontalBarChart): HorizontalBarChart {
+            // hide all lines of chart
+            chart.axisLeft.setDrawLabels(false)
+            chart.axisLeft.setDrawGridLines(false)
+            chart.axisLeft.setDrawAxisLine(false)
+            chart.axisRight.setDrawLabels(false)
+            chart.axisRight.setDrawGridLines(false)
+            chart.axisRight.setDrawAxisLine(false)
+            chart.xAxis.setDrawAxisLine(false)
+            chart.xAxis.setDrawGridLines(false)
+            // set labels for bars
+//            chart.xAxis.granularity = 1f
+//            chart.xAxis.isGranularityEnabled = true
+//            chart.xAxis.setCenterAxisLabels(false)
+            chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+            chart.xAxis.valueFormatter = IndexAxisValueFormatter(Constants.MONTHS_LABEL.asReversed())
+            // hide description
+            chart.description.text = ""
+            // hide legend
+            chart.legend.isEnabled = false
+            chart.setFitBars(true)
             return chart
         }
     }
