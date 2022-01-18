305fd24d3e36a2982ee0ce969499b5dc1718afed
jangacnik <gacnik.jan@gmail.com>
2022-01-18 16:44:48 +0100

    Updated color scheme and themes

--- app/src/main/java/com/example/diplomskanaloga/activities/MainActivity.kt
+++ app/src/main/java/com/example/diplomskanaloga/activities/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.diplomskanaloga.activities
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,26 +14,20 @@ import com.example.diplomskanaloga.models.Employee
 import com.example.diplomskanaloga.services.EmployeeRestService
 import com.example.diplomskanaloga.utils.ChartUtils
 import com.example.diplomskanaloga.utils.ChartValueFormatter
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.HorizontalBarChart
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.formatter.IAxisValueFormatter
-import com.github.mikephil.charting.formatter.IValueFormatter
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.google.gson.Gson
 
 
+
+
+
 class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
     lateinit var employeeRestService: EmployeeRestService
     lateinit var userData: Employee
-
     // elements
     lateinit var welcomeTextView: TextView
 
@@ -43,17 +36,17 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        welcomeTextView = findViewById(R.id.welcome_text)
+//        welcomeTextView = findViewById(R.id.welcome_text)
         employeeRestService = EmployeeRestService()
         barchartTest(R.id.weekly_bar_chart)
         horizontalBarChartText(R.id.yearly_overview_chart)
+        isUserLoggedIn()
     }
 
     fun isUserLoggedIn() {
         val sharedPreferences =
             this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
         // if user isn't logged in forward to auth activity else dashboard
-        sharedPreferences.getString("token", "")?.let { Log.e("log", it) }
         if (!sharedPreferences.contains(getString(R.string.preference_token))) {
             val intent = Intent(this, AuthenticationActivity::class.java).apply {
             }
@@ -84,6 +77,7 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         entries.add(BarEntry(4f, 10f))
         entries.add(BarEntry(5f, 70f))
         val barData = BarDataSet(entries, null)
+        barData.color = resources.getColor(R.color.secondaryColor)
         val data = BarData(barData)
         data.barWidth = 0.9f
         val chart: BarChart = ChartUtils.setBarChartDefaults(findViewById<BarChart>(id))
@@ -91,25 +85,12 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         chart.animateXY(1000, 1000)
         // set text size of bar values
         chart.data.setValueTextSize(14f)
+
         chart.animate()
     }
 
     fun horizontalBarChartText(id: Int) {
         val entries: MutableList<BarEntry> = ArrayList()
-//        entries.add(BarEntry(0f, 200f))
-//        entries.add(BarEntry(1f, 200f))
-//        entries.add(BarEntry(2f, 200f))
-//        entries.add(BarEntry(3f, 200f))
-//        entries.add(BarEntry(4f, 200f))
-//        entries.add(BarEntry(5f, 200f))
-//        entries.add(BarEntry(6f, 200f))
-//        entries.add(BarEntry(7f, 200f))
-//        entries.add(BarEntry(8f, 200f))
-//        entries.add(BarEntry(9f, 200f))
-//        entries.add(BarEntry(10f, 200f))
-//        entries.add(BarEntry(11f, 200f))
-
-
         entries.add(BarEntry(0f, 168f))
         entries.add(BarEntry(1f, 141f))
         entries.add(BarEntry(2f, 155f))
@@ -124,6 +105,7 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         entries.add(BarEntry(11f, 158f))
         val valueFormatter = ChartValueFormatter("", " h", Constants.MONTHS_LABEL)
         val barData = BarDataSet(entries, null)
+        barData.color = resources.getColor(R.color.secondaryColor)
         val data = BarData(barData)
         data.setValueFormatter(valueFormatter)
         data.barWidth = 0.9f
@@ -143,37 +125,10 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         chart.animate()
     }
 
-    fun lineChartTest() {
-
-        val entries: MutableList<Entry> = ArrayList()
-        entries.add(Entry(0f, 44f))
-        entries.add(Entry(1f, 11f))
-        entries.add(Entry(2f, 22f))
-        entries.add(Entry(3f, 34f))
-        entries.add(Entry(4f, 47f))
-        val lineDataSet = LineDataSet(entries, "test")
-        lineDataSet.lineWidth = 4f
-        lineDataSet.color = Color.CYAN
-        lineDataSet.setDrawCircles(false)
-        lineDataSet.setDrawHighlightIndicators(true)
-        lineDataSet.isHighlightEnabled = true
-        lineDataSet.highLightColor = Color.CYAN
-        lineDataSet.valueTextColor = Color.DKGRAY
-        lineDataSet.mode = LineDataSet.Mode.STEPPED
-        val chart = findViewById<LineChart>(R.id.yearly_overview_chart)
-        chart.data = LineData(lineDataSet)
-        chart.invalidate()
-    }
-
     override fun onValueSelected(e: Entry?, h: Highlight?) {
         Log.w("values", e.toString())
     }
 
     override fun onNothingSelected() {
     }
-}
-
-class MyValueFormatter() : ValueFormatter() {
-
-
 }
