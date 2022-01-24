b073d40c05cb13c33c806cc64d30f69a94fbebdd
jangacnik <gacnik.jan@gmail.com>
2022-01-24 11:32:05 +0100

    Added bottom navigation activity

--- app/src/main/java/com/example/diplomskanaloga/activities/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/diplomskanaloga/activities/ui/home/HomeFragment.kt
@@ -1,14 +1,21 @@
-package com.example.diplomskanaloga.activities
+package com.example.diplomskanaloga.activities.ui.home
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.android.volley.VolleyError
 import com.example.diplomskanaloga.Constants
 import com.example.diplomskanaloga.R
+import com.example.diplomskanaloga.activities.AuthenticationActivity
+import com.example.diplomskanaloga.databinding.FragmentHomeBinding
 import com.example.diplomskanaloga.interfaces.VolleyResponse
 import com.example.diplomskanaloga.models.Employee
 import com.example.diplomskanaloga.services.EmployeeRestService
@@ -16,59 +23,87 @@ import com.example.diplomskanaloga.utils.ChartUtils
 import com.example.diplomskanaloga.utils.ChartValueFormatter
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.HorizontalBarChart
-import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.google.gson.Gson
 
+class HomeFragment : Fragment(), OnChartValueSelectedListener {
 
-
-
-
-class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
     lateinit var employeeRestService: EmployeeRestService
     lateinit var userData: Employee
     // elements
     lateinit var welcomeTextView: TextView
 
+    lateinit var barChart: BarChart
+    lateinit var horizontalBarChart: HorizontalBarChart
+
     // util
     val gson = Gson()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        welcomeTextView = findViewById(R.id.welcome_text)
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         employeeRestService = EmployeeRestService()
-        barchartTest(R.id.weekly_bar_chart)
-        horizontalBarChartText(R.id.yearly_overview_chart)
-//        isUserLoggedIn()
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        barChart = binding.weeklyBarChart
+        horizontalBarChart = binding.yearlyOverviewChart
+
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        barchartTest()
+        horizontalBarChartText()
+        return root
     }
 
-    fun isUserLoggedIn() {
-        val sharedPreferences =
-            this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-        // if user isn't logged in forward to auth activity else dashboard
-        if (!sharedPreferences.contains(getString(R.string.preference_token))) {
-            val intent = Intent(this, AuthenticationActivity::class.java).apply {
-            }
-            startActivity(intent)
-        } else {
-            employeeRestService.getUserData(this, object : VolleyResponse {
-                override fun onSuccess(response: Any?) {
-                    userData = gson.fromJson(response.toString(), Employee::class.java)
-                    welcomeTextView.append(", " + userData.name)
-                    Log.w("Response", userData.toString())
-                }
-
-                override fun onError(error: VolleyError?) {
-                    if (error != null) {
-                        Log.e("Response Error", error.networkResponse.toString())
-                    }
-                }
-            })
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
-    fun barchartTest(id: Int) {
+//    fun isUserLoggedIn() {
+//        val sharedPreferences =
+//            this.context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+//        // if user isn't logged in forward to auth activity else dashboard
+//        if (!sharedPreferences.contains(getString(R.string.preference_token))) {
+//            val intent = Intent(this, AuthenticationActivity::class.java).apply {
+//            }
+//            startActivity(intent)
+//        } else {
+//            employeeRestService.getUserData(this, object : VolleyResponse {
+//                override fun onSuccess(response: Any?) {
+//                    userData = gson.fromJson(response.toString(), Employee::class.java)
+//                    welcomeTextView.append(", " + userData.name)
+//                    Log.w("Response", userData.toString())
+//                }
+//
+//                override fun onError(error: VolleyError?) {
+//                    if (error != null) {
+//                        Log.e("Response Error", error.networkResponse.toString())
+//                    }
+//                }
+//            })
+//        }
+//    }
+
+    fun barchartTest() {
         val entries: MutableList<BarEntry> = ArrayList()
         entries.add(BarEntry(0f, 30f))
         entries.add(BarEntry(1f, 120f))
@@ -80,7 +115,7 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         barData.color = resources.getColor(R.color.secondaryColor)
         val data = BarData(barData)
         data.barWidth = 0.9f
-        val chart: BarChart = ChartUtils.setBarChartDefaults(findViewById<BarChart>(id))
+        val chart: BarChart = ChartUtils.setBarChartDefaults(barChart)
         chart.data = data
         chart.animateXY(1000, 1000)
         // set text size of bar values
@@ -89,7 +124,7 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         chart.animate()
     }
 
-    fun horizontalBarChartText(id: Int) {
+    fun horizontalBarChartText() {
         val entries: MutableList<BarEntry> = ArrayList()
         entries.add(BarEntry(0f, 168f))
         entries.add(BarEntry(1f, 141f))
@@ -110,7 +145,7 @@ class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
         data.setValueFormatter(valueFormatter)
         data.barWidth = 0.9f
         val chart: HorizontalBarChart =
-            ChartUtils.setHorizontalBarChartDefault(findViewById<HorizontalBarChart>(id))
+            ChartUtils.setHorizontalBarChartDefault(horizontalBarChart)
         chart.xAxis.setLabelCount(entries.size, false)
         chart.data = data
         chart.data.setValueTextSize(14f)
